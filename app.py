fd106801db01459b01fe277177319cc551750d2e
Alvin Haruna <harunaalvin69@gmail.com>
2023-03-06 18:10:43 +0100

    added comments on steps to follow in building app

---
+++ app.py
@@ -0,0 +1,5 @@
+# function that takes list & target
+# variables (middle, start, end, steps)
+# recursion or while loop
+# increase steps each time a split is done
+# conditions to track target position
